1244146aaaf14d16cce40d80ab8d6a1ca7940a39
Alex Wu <dindinw@users.noreply.github.com>
2018-02-03 04:17:00 +0800

    remove bigint and byte

--- GpuMiner/Core/Common.h
+++ GpuMiner/Core/Common.h
@@ -27,6 +27,4 @@ namespace XDag
         o << _t;
         return o.str();
     }
-    using bigint = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<>>;
-    using byte = uint8_t;
 }
